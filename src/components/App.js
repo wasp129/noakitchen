a398ef5a73da3b5132e94789ad7303bf4ae6363f
Jacob Packert <jacobpackert@gmail.com>
2019-10-01 19:51:52 +0200

    add data url

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,14 @@ import './App.scss';
 import Menu from './Menu/Menu';
 import Nav from './Nav/Nav';
 
+let url="https://spreadsheets.google.com/feeds/list/1DkFyeKVHJ4il9Z_MxT9deFf_D1uqDiNKjopiTLqnhd8/od6/public/values?alt=json";
+
 function App() {
+
+  // fetch(url);
+  // async await fetch?
+
+
   return ( <>
     <Nav></Nav>
     <Menu></Menu>
