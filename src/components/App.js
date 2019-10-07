aa82f9d9c361a4353492725869ba51dd1fedb3b7
Jacob Packert <jacobpackert@gmail.com>
2019-10-07 22:09:32 +0200

    change color of active day

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import axios from "axios";
 import './App.scss';
 import Menu from './Menu/Menu';
-// import Nav from './Nav/Nav';
 
 
 function App() {
   return ( 
     <div className="appwrapper">
-    {/* <Nav></Nav> */}
     <Menu></Menu>
     </div>
   );
