d33b583281970a52a6eb247beba0efb23f0e36f0
Jacob Packert <jacobpackert@gmail.com>
2019-10-08 21:21:40 +0200

    add fonts

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,9 @@ import Menu from './Menu/Menu';
 function App() {
   return ( 
     <div className="appwrapper">
-      <Marquee text="THIS WEEK"></Marquee>
+      <Marquee text="This Week"></Marquee>
       <Menu week="thisweek"></Menu>
-      <Marquee text="NEXT WEEK"></Marquee>
+      <Marquee text="Next Week"></Marquee>
       <Menu week="nextweek"></Menu>
 
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './fonts/AftenScreen-Regular.ttf';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
